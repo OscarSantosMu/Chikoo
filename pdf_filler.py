d21c58ff6e7a60691a1d5063b9793b4fc3a07859
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-01-24 19:20:48 +0000

    finished fill_familiary_history function

--- pdf_filler.py
+++ pdf_filler.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from PIL import Image, ImageDraw, ImageFont
 
 # Fonts
@@ -32,6 +33,64 @@ def fill_pdf(data: dict) -> Image:
     draw.text((525, 301), data['pc'], fill=(0, 0, 0), font=regular_font)
     draw.text((558, 342), data['work_status'],fill=(0, 0, 0), font=regular_font)
 
+    # Familiar History
+    fill_familiar_history(draw, 'cancer', 'father')
+    fill_familiar_history(draw, 'alcoholism', 'father')
+    fill_familiar_history(draw, 'arthritis', 'father')
+    fill_familiar_history(draw, 'depression', 'father')
+    fill_familiar_history(draw, 'diabetes', 'father')
+    fill_familiar_history(draw, 'obesity', 'father')
+    fill_familiar_history(draw, 'pressure', 'father')
+    fill_familiar_history(draw, 'smoking', 'father')
+    fill_familiar_history(draw, 'smoking', 'father')
+    fill_familiar_history(draw, 'other', 'father', 'bipolarity')
+
     image.save("chikoo.png")
 
     return image
+
+
+def fill_familiar_history(draw: ImageDraw, type: Optional[str] = None, familiar: Optional[str] = None, other: Optional[str] = None):
+    """
+        Fills with an X the disease that one of the familiars had.
+
+        type: Optional[str] = None by default, represents the type of disease that a familiar had 
+        familiar: Optional[str] = None by default, represents the familiar who had a disease
+        other: Optional[str] = None by default, represents if a familiar had another kind of disease or hederitary disease
+    """
+    types = {
+        'alcoholism': 450,
+        'arthritis': 465,
+        'cancer': 480,
+        'hearth':495,
+        'depression': 510,
+        'diabetes': 525,
+        'obesity': 540,
+        'pressure': 555,
+        'smoking': 570,
+        'other': 585,
+    }
+
+    familiars = {
+        'mother': 178,
+        'father': 274,
+        'brothers': 370,
+        'grand_parents': 466,
+        'patient_doesnt_know': 620,
+    }
+
+    if familiar:
+        if other:
+            draw.text(
+                (familiars[familiar]-40, types[type]),
+                other,
+                fill=(137, 87, 254),
+                font=regular_font
+            )
+        else:
+            draw.text(
+                (familiars[familiar], types[type]),
+                'X',
+                fill=(137, 87, 254), font=regular_font
+            )
+    return
