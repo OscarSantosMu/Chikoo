0a66c15bc4e295a4721f6082f7f553e597b0fcb1
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-01-25 19:08:42 +0000

    completed the pdf fillment

--- pdf_filler.py
+++ pdf_filler.py
@@ -22,16 +22,65 @@ def fill_pdf(data: dict) -> Image:
         draw.text((521, 126), data['name'], fill=(0, 0, 0), font=regular_font)
 
     # Personal Data
-    draw.text((120, 196), data['name'], fill=(0, 0, 0), font=regular_font)
-    draw.text((135, 227), data['last_name'], fill=(0, 0, 0), font=regular_font)
-    draw.text((120, 266), data['birth_date'],fill=(0, 0, 0), font=regular_font)
-    draw.text((120, 301), data['adress'], fill=(0, 0, 0), font=regular_font)
-    draw.text((168, 342), data['phone_number'],fill=(0, 0, 0), font=regular_font)
-    draw.text((537, 196), data['sex'], fill=(0, 0, 0), font=regular_font)
-    draw.text((537, 227), data['age'], fill=(0, 0, 0), font=regular_font)
-    draw.text((386, 266), data['place_of_birth'],fill=(0, 0, 0), font=regular_font)
-    draw.text((525, 301), data['pc'], fill=(0, 0, 0), font=regular_font)
-    draw.text((558, 342), data['work_status'],fill=(0, 0, 0), font=regular_font)
+    draw.text(
+        (120, 196),
+        data['name'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
+    draw.text(
+        (135, 227),
+        data['last_name'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
+    draw.text(
+        (120, 266),
+        data['birth_date'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
+    draw.text(
+        (120, 301),
+        data['adress'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
+    draw.text(
+        (168, 342),
+        data['phone_number'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
+    draw.text(
+        (537, 196),
+        data['sex'],
+        fill=(0, 0, 0),
+        font=regular_font)
+    draw.text(
+        (537, 227),
+        data['age'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
+    draw.text(
+        (386, 266),
+        data['place_of_birth'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
+    draw.text(
+        (525, 301),
+        data['pc'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
+    draw.text(
+        (558, 342),
+        data['work_status'],
+        fill=(0, 0, 0),
+        font=regular_font
+    )
 
     # Familiar History
     fill_familiar_history(draw, 'cancer', 'father')
@@ -45,6 +94,13 @@ def fill_pdf(data: dict) -> Image:
     fill_familiar_history(draw, 'smoking', 'father')
     fill_familiar_history(draw, 'other', 'father', 'bipolarity')
 
+    # Non Pathological History
+    food = [('milk',1),('meat',1),('eggs',1),('vegetables',1),('cereals',1)]
+    fill_non_pathologic(draw, food, 'food')
+    fill_non_pathologic(draw, 1,'activity')
+    fill_non_pathologic(draw, 1,'tabaco')
+    fill_non_pathologic(draw, 1,'alcohol')
+    fill_non_pathologic(draw, 1,'drugs')
     image.save("chikoo.png")
 
     return image
@@ -62,7 +118,7 @@ def fill_familiar_history(draw: ImageDraw, type: Optional[str] = None, familiar:
         'alcoholism': 450,
         'arthritis': 465,
         'cancer': 480,
-        'hearth':495,
+        'hearth': 495,
         'depression': 510,
         'diabetes': 525,
         'obesity': 540,
@@ -93,4 +149,30 @@ def fill_familiar_history(draw: ImageDraw, type: Optional[str] = None, familiar:
                 'X',
                 fill=(137, 87, 254), font=regular_font
             )
+    return
+
+
+def fill_non_pathologic(draw: ImageDraw, times_per_week, type):
+    food_conversion = {'milk': 0, 'meat': 1,
+                       'eggs': 2, 'vegetables': 3, 'cereals': 4}
+    x_positions = {0: 173, 1: 270, 2: 367, 3: 456, 4: 615}
+    y_positions = {'food': 712, 'activity': 816,
+                   'tabaco': 920, 'alcohol': 1016, 'drugs': 1120}
+    
+    # Food fillment
+    if type == "food":
+        for time in times_per_week:
+            print(time[0],time[1])
+            draw.text((0,0),'a',fill=(0,0,0))
+            draw.text(
+                (x_positions[food_conversion[time[0]]], y_positions['food']),
+                str(time[1]),
+                fill=(137, 87, 254),
+                font=regular_font
+            )
+        return
+
+    print(x_positions[times_per_week])
+    draw.text((x_positions[times_per_week], y_positions[type]),str(times_per_week),fill=(137, 87, 254),font=regular_font)
+
     return
